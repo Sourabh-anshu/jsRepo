50f27f9d052c3bdf37b63a3711c0dc79338a2df4
Sourabh Kumar Singh <87141360+Sourabh-anshu@users.noreply.github.com>
2024-02-16 20:09:52 +0000

    variables and constants added

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+const a = 1234
+let b = "utility"
+var c = 9980
+d = "yielding"
+let e
+console.log("Before Changes");
+console.table([a,b,c,d,e])
+// a=33
+b="lityuily"
+c = 0008
+d = "453"
+e=  "p"
+console.log("After Changes");
+console.table([a,b,c,d,e])
