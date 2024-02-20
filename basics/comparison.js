3634e40ad66af6665586ac6392b068602df6e1a5
Sourabh Kumar Singh <87141360+Sourabh-anshu@users.noreply.github.com>
2024-02-20 13:34:29 +0000

    comparison added

---
+++ basics/comparison.js
@@ -0,0 +1,9 @@
+// console.log("2" == 2);
+// console.log("1" == true);
+// console.log(false == "0");
+// console.log("03" < 10);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null <= 0);
+
+console.log(1 === true);
