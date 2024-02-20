babc663963a764be3362b93f1ce11f7e69c381e2
Sourabh Kumar Singh <87141360+Sourabh-anshu@users.noreply.github.com>
2024-02-20 13:00:37 +0000

    datatypeeeCconversion Pushed

---
+++ basics/datatypeConversion.js
@@ -0,0 +1,34 @@
+let a = -22
+let aInString = String(a)
+// console.log(aInString);
+// console.log(typeof aInString);
+
+let aInBoolean = Boolean(a)
+// console.log(aInBoolean);
+// console.log(typeof(aInBoolean));
+
+let b = "string22"
+let bInNumber = Number(b)
+
+// console.log(bInNumber);
+// console.log(typeof bInNumber);
+
+let c = undefined
+let cInString = String(c)
+
+// console.log(cInString);
+// console.log(typeof cInString);
+
+let d = null
+// let dInNumber = Number(d)
+let dInString = String(d)
+
+// console.log(dInString);
+// console.log(typeof(dInString));
+
+// let e = "eeew"
+let e = ""
+let eInBoolean = Boolean(e)
+
+console.log(eInBoolean);
+console.log(typeof(eInBoolean));
