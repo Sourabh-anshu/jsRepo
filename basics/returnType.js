f95d946eab733ac4004eed3fb957b5c488571ce5
Sourabh Kumar Singh <87141360+Sourabh-anshu@users.noreply.github.com>
2024-02-20 14:05:48 +0000

    Return type added

---
+++ basics/returnType.js
@@ -0,0 +1,26 @@
+let aNumber = 44
+let aString = "World"
+let aBool = false
+let aNull = null
+let und = undefined
+let aSymbol = Symbol("234")
+let aBigNumber = 9999999999999999999n
+
+let fruits = ["apple", "mango", "guava"]
+let aObj = {
+    id : 9980,
+    sal : "28k",
+    exp : false
+}
+
+let aFunc = function(){
+    console.log("hii");
+}
+
+// console.log(typeof aNumber);
+// console.log(typeof aNull);
+// console.log(typeof aSymbol);
+// console.log(typeof und);
+// console.log(typeof aBigNumber);
+// console.log(typeof fruits);
+console.log(typeof aFunc);
